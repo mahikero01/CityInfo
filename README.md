5848ee9dd2896606093ddbecec5c432ae92aa634
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-11-26 06:49:25 +0800

    up

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ $ dotnet new projectType -> create a new project
    - webapi     ASP .NET Core Web API
    - web        ASP .NET Core empty
    
-$ dotnet add package Microsoft.AspNetCore.Mvc.Core --version 2.0.1   -> install asp .net mvc core dependency
+$ dotnet add package Microsoft.AspNetCore.Mvc.Core --version 2.0.1   -> install asp .net mvc core dependencydotnet $ $ dotnet add package Microsoft.AspNetCore.Mvc.Formatters.Xml --version 2.0.1  -> install xml formatter 
+
 
 $ dotnet run -> deploy project
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace CityInfo
 {
@@ -15,7 +17,22 @@ namespace CityInfo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+            .AddMvcOptions(o => o.OutputFormatters.Add(
+                //This option will add XML output formatter option
+                new XmlDataContractSerializerOutputFormatter()
+            ));
+            
+
+            //THis is only use if you want the properties in JSON are Sentenced Case
+            // .AddJsonOptions(o => {
+            //     if (o.SerializerSettings.ContractResolver != null) 
+            //     {
+            //         var castedResolver = o.SerializerSettings.ContractResolver
+            //                 as DefaultContractResolver;
+            //             castedResolver.NamingStrategy = null;
+            //     }
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
