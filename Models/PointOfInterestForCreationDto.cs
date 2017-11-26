4b70d070662f440735468f039e95304bc1af9b8a
Sarmiento <sarmife@prd.manulifeusa.com>
2017-11-26 06:43:57 -0500

    up

--- Models/PointOfInterestForCreationDto.cs
+++ Models/PointOfInterestForCreationDto.cs
@@ -1,3 +1,8 @@
 namespace CityInfo.API.Models
 {
+    public class PointOfInterestForCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
 }
